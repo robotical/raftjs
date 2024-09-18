6a63bcb54b9cea7fea88a1e606de085614ed049a
NikTheGeek1 <ntheodoropoulos@outlook.com>
2024-09-18 19:32:10 +0300

    removing dist

--- dist/RaftCustomAttrHandler.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-import { DeviceTypePollRespMetadata } from "./RaftDeviceInfo";
-export default class CustomAttrHandler {
-    handleAttr(pollRespMetadata: DeviceTypePollRespMetadata, msgBuffer: Buffer, msgBufIdx: number): number[][];
-}
